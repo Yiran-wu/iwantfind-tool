0459065fa81478bf5c6a946cee95b5e3d16bf739
yiran <yiranpinkroom@gmail.com>
2016-11-26 01:43:34 +0800

    add submit shell

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 spark-submit  --class com.iwantfind.Analyser  /Users/renbaoling/Documents/workspace/iwantfind-tool/target/iwantfind-tool-0.1-SNAPSHOT.jar   /Users/renbaoling/Documents/workspace/iwantfind-tool/target/
+
+
+spark-submit --master yarn --deploy-mode client --name type   --class com.iwantfind.ASAnalyser --num-executors 10 --executor-memory 2g  /tmp/iwantfind-tool-0.1-SNAPSHOT.jar /testdata
